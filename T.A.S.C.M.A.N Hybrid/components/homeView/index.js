d9de4a468ac56b004cb7796433d9fbc132e87f09
Nana Arkorful <nana.arkorful.12@cnu.edu>
2015-10-27 18:53:14 +0000

    Changes made by Nana Arkorful (nana.arkorful.12@cnu.edu). Got data binding on listview to work

--- T.A.S.C.M.A.N Hybrid/components/homeView/index.js
+++ T.A.S.C.M.A.N Hybrid/components/homeView/index.js
@@ -1,31 +1,18 @@
 'use strict';
 
 var sportsDataSource = [];
+var phaseDataSource = [];
 
 app.homeView = kendo.observable({
     onShow: function() {},
     afterShow: function() {},
-    /*selectedProduct: null,
-    sports: new kendo.data.DataSource({
-          transport: {
-            read:  {
-              url: "http://demos.telerik.com/kendo-ui/service/products",
-              dataType: "jsonp" // "jsonp" is required for cross-domain requests; use "json" for same-domain requests
-            },
-          schema: {
-            model: { id: "ProductID", name:"ProductName" }
-          }
-	});
-    sports: [
-        { id: 1, name: "Coffee" },
-        { id: 2, name: "Tea" },
-        { id: 3, name: "Juice" }
-    ] */
-    
     sportsDataSource : sportsDataSource,
+    phaseDataSource: phaseDataSource,
     selectedSportId: null,
         onChange: function(e) {
-            console.log(this.get("selectedSportId"));
+            //console.log(this.get("selectedSportId"));
+            //console.log(this.get("selectedPhaseId"));
+            console.log($("#dropdown").val());
     }
 });
 
@@ -37,11 +24,22 @@ $.ajax({
         success: function(data) {
             console.log(data);
             app.homeView.set("sportsDataSource", data);
-            app.homeView.set("selectedSportId", 3);
+            app.homeView.set("selectedSportId", 0);
+        }
+});
+
+
+$.ajax({
+        type: 'GET',               
+        url: "http://unkkbeba62de.nanaarkorful12.koding.io/service/retrieve_phases.php?sportid=4",
+        dataType : "json",
+        context: this,
+        success: function(data) {
+            console.log(data);
+            app.homeView.set("phaseDataSource", data);
         }
 });
 kendo.bind($("select"),app.homeView);
 // START_CUSTOM_CODE_homeView
 
-//kendo.bind($("select"), viewModel);
 // END_CUSTOM_CODE_homeView
